c3a40a350f81ae5b66896fe68f1a81b22cf6f07d
hostmorke <hostmorke@hostmorke.io>
2017-03-01 13:35:49 +0200

    css parsing

--- psyanimal.py
+++ psyanimal.py
@@ -1,26 +1,33 @@
 import json
-from bottle import route, run, template, request, error
+from bottle import route, run, template, request, error, static_file
 
 with open('questions.json', 'r') as jsonfile:
     questions_dict = json.load(jsonfile)
+
+@route('/<filename:re:.*\.(css|ico)>')
+def send_static(filename):
+    return static_file(filename, root='./')
 
 @route('/')
 def ddt():
     return template('''
         <!DOCTYPE html>
-        <html><head><meta charset="utf-8"><title>Тест "Несуществующее животное"</title></head>
-        <body bgcolor="#558000">
+        <html><head><meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="stylesheet.css">
+        <title>Тест "Несуществующее животное"</title></head>
         <h1>Тест Несуществующее животное</h1><br>
-        <h4>
         <form action="/" method=post>
+            <h3>
             Имя: <input name="name" type="text" />
             Описание: <input name="desc" type="text" /><br><br>
+            </h3>
+            <h4>
             % for key in sorted(questions_dict.keys()):
                 <input type="checkbox" name="question" value="{{key}}"> {{questions_dict[key][0]}}<br>
             % end
+            </h4>
             <br>
             <input value="Проверить" type="submit" />
-        </h3>
         </form></body></html>
     ''', questions_dict=questions_dict)
 
@@ -31,16 +38,19 @@ def ddt_result():
     data = request.forms.getall('question')
     return template('''
         <!DOCTYPE html>
-        <html><head><meta charset="utf-8"><title>Тест "Несуществующее животное"</title></head>
-        <body bgcolor="#558000">
+        <html><head><meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="stylesheet.css">
+        <title>Тест "Несуществующее животное"</title></head>
         <h3>Имя: {{name}}<br>
         Описание: {{desc}}<br></h3>
+        <h4>
         <ul>
         % for key in data:
             <li><b>{{questions_dict[key][0]}}</b><br>
             {{questions_dict[key][1]}}</li>
         %end
         </ul>
+        </h4>
         <br>
         <form action="http://127.0.0.1:9999">
             <input type="submit" value="Назад" />
